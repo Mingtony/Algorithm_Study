235f6bc05c9490ff648cdf4145201b42f7aba66b
Mingtony <junhyukkim92@gmail.com>
2021-03-25 13:02:39 +0900

    [BOJ] 11654 아스키 코드

---
+++ BOJ/BOJ_11654.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+public class BOJ_11654 {
+
+    public static void main(String[] args) throws IOException {
+        // TODO Auto-generated method stub
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        char tmp = br.readLine().charAt(0);
+        System.out.println((int)tmp);
+    }
+}
