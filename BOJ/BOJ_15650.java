e2bf9e448e0dbb3cf26dc0842fa38f0826aae8e7
Mingtony <junhyukkim92@gmail.com>
2021-04-01 13:12:56 +0900

    [BOJ] 15652 N과 M(4)(Backtracking)

---
+++ BOJ/BOJ_15650.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+public class BOJ_15650 {
+    static int[] tmp;
+    static StringBuilder sb;
+    static void backtracking(int st,int depth, int N, int std){
+        if(depth == std){
+            for(int n : tmp)
+                sb.append(n+" ");
+            sb.append("\n");
+            return;
+        }
+        for(int i=st;i<=N;i++){
+            tmp[depth] = i;
+            backtracking(i,depth+1,N,std);
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        // TODO Auto-generated method stub
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N,M;
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        sb = new StringBuilder();
+        tmp = new int[M];
+
+        backtracking(1,0,N,M);
+        System.out.println(sb);
+    }
+}
+
+
