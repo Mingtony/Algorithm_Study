655f23a6e5f392872b5e994a6a054ce4ce831537
Mingtony <junhyukkim92@gmail.com>
2021-04-02 20:00:35 +0900

    [BOJ] 15728번 에리-카드

--- BOJ/BOJ_15728.java
+++ BOJ/BOJ_15728.java
@@ -5,34 +5,52 @@ import java.util.*;
 
 
 public class BOJ_15728 {
+    static int N,K;
+    static ArrayList<Integer> share, team;
 
     public static void main(String[] args) throws IOException {
         // TODO Auto-generated method stub
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
-        int N = Integer.parseInt(br.readLine());
-        int[] btn = new int[N+2];
-        int cnt=0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        share = new ArrayList<>();
+        team = new ArrayList<>();
         st = new StringTokenizer(br.readLine());
         for(int i=0;i<N;i++)
-            btn[i] = Integer.parseInt(st.nextToken());
+            share.add(Integer.parseInt(st.nextToken()));
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<N;i++)
+            team.add(Integer.parseInt(st.nextToken()));
 
-        for(int i=0;i<N;i++) {
-            if(btn[i]==1) {
-                cnt++;
-                if(btn[i+1]==0)
-                    btn[i+1] = 1;
-                else
-                    btn[i+1]=0;
-                if(btn[i+2]==0)
-                    btn[i+2] = 1;
-                else
-                    btn[i+2]=0;
+//        //음수 * 음수 고를 경우 가장 큰 경우가 나올 수 있음(정렬 후 그리디 방식은 안됨!)
+//        Arrays.sort(share);
+//        Arrays.sort(team);
+//        System.out.println(share[N-1]*team[N-K-1]);
+
+        //최선의 방법 K장 골라 빼기
+        int max = 0, idx = -1;
+        for(int i=0;i<K;i++){
+            max = Integer.MIN_VALUE;
+            idx = -1;
+            for(int j=0;j<N;j++){
+                for(int k=0;k<team.size();k++){
+                    if(max < share.get(j)*team.get(k)){
+                        max = share.get(j) * team.get(k);
+                        idx = k;
+                    }
+                }
+            }
+            team.remove(idx);
+        }
+        //max 값 찾기
+        max = share.get(0)*team.get(0);
+        for(int i=1;i<N;i++){
+            for(int j=0;j<team.size();j++){
+                max = share.get(i)*team.get(j)>max ? share.get(i)*team.get(j):max;
             }
         }
-        System.out.println(cnt);
+        System.out.println(max);
     }
 }
-
-
